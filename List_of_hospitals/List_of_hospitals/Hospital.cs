6cd4f6d4a8bc83089b6593f6f8314c0a9cf27fc0
Anna Dzyuba <annadzyuba200135@gmail.com>
2025-09-15 11:44:48 +0300

    Add List_of_hospitals project

---
+++ List_of_hospitals/List_of_hospitals/Hospital.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_of_hospitals
+{
+    public class Hospital
+    {
+        private readonly int _id;
+        private readonly string _hospitalName;
+
+        public Hospital(int id, string hospitalName)
+        {
+            _id = id;
+            _hospitalName = hospitalName;
+        }
+
+        public int Id 
+        { 
+            get { return _id; } 
+        }
+
+        public string HospitalName
+        {
+            get { return _hospitalName; }
+        }
+
+    }
+}
+
